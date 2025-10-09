5299e986bca5aafa77d130ca800f3bfe9bada89c
Krishna Murthy Praveen (AE/MFD1) <Praveen.KrishnaMurthy2@de.bosch.com>
2025-10-09 15:03:59 +0200

    Enhance Person class with method overloads and additional functionality

--- scala-tut2/src/main/scala/oop/MethodNotations.scala
+++ scala-tut2/src/main/scala/oop/MethodNotations.scala
@@ -6,6 +6,13 @@ object MethodNotations extends App{
 
         def likes(movie: String): Boolean = movie == favoriteMovie
         def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+        def +(nickName: String): Person = new Person(s"${this.name} ($nickName)", favoriteMovie)
+        def unary_+ : Person = new Person(name, favoriteMovie) // new person with age incremented
+        def learns(subject: String): String = s"$name learns $subject"
+        def learnsScala: String = this learns "Scala"
+        def apply(times: Int): String = s"$name watched $favoriteMovie $times times"
+        def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+
     }
 
     val mary = new Person("Mary", "Inception")
@@ -31,8 +38,17 @@ object MethodNotations extends App{
 
     // post-fix notation, may give ambiguous code
     // apply-Method - special method, no parameters
-    /* 
-        
+    /*
+        1. Overload the + operator
+            mary + "the rockstar" => new person "Mary (the rockstar)"
+        2. Add an age to the Person class
+            Add a unary + operator => new person with the age + 1
+            +mary => mary with the age incrementer
+        3. Add a "learns" method in the person class => "Mary learns Scala"
+            Add a learnsScala method, calls learns method with "Scala"
+            Use it in postfix notation
+        4. Overload the apply method
+            mary.apply(2) => "Mary watched Inception 2 times"
      */
 
 
